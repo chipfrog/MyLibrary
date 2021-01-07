ab3333c0f06990415219b75d832361a6f6ca8669
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2021-01-07 17:12:18 +0200

    changed review form appearance, book read checkbox

--- front-end/src/Services/books.js
+++ front-end/src/Services/books.js
@@ -19,7 +19,8 @@ export const addBook = async ( bookInfo, token ) => {
     author: bookInfo.volumeInfo.authors[0],
     linkToCoverImage: bookInfo.volumeInfo.imageLinks.thumbnail,
     review: bookInfo.review,
-    rating: bookInfo.rating
+    rating: bookInfo.rating,
+    read: bookInfo.read
   }
 
   const response = await axios.post(`${baseUrl}/api/googlebooks`, bookData, config)
--- front-end/src/Views/BookInfo.js
+++ front-end/src/Views/BookInfo.js
@@ -12,6 +12,8 @@ const BookInfo = () => {
   const [open, setOpen] = useState(false)
   const [review, setReview] = useState('')
   const [rating, setRating] = useState(1)
+  const [read, setRead] = useState(false)
+  const [quotes, setQuotes] = useState([])
   
 
   if (bookInfo === null) {
@@ -26,7 +28,8 @@ const BookInfo = () => {
     const test = {
       ...bookInfo,
       rating: rating,
-      review: review
+      review: review,
+      read: read
     }
     console.log(test)
 
@@ -34,6 +37,8 @@ const BookInfo = () => {
   }
   // Korjaa näkymä, kun kirjalijoita enemmän kuin yksi!
   
+  // className="pt-4 text-center"
+
   return (
     <Container className="pt-4 text-center">
       <h2>{bookInfo.volumeInfo.title}</h2>
@@ -47,57 +52,54 @@ const BookInfo = () => {
           {bookInfo.volumeInfo.description}
         </Col>
       </Row>
-      <Row>
+      <Row className="pt-4">
         <Col>
           <Button onClick={() => setOpen(!open)} aria-controls="collapseForm">
             Add to Library
           </Button>
-          {/* <Button onClick={() => handleBookAdding()}>
-            Add to Library
-          </Button> */}
         </Col>
         <Col>
         </Col>
+      </Row>  
         <Collapse in={open} >
-          <Form onSubmit={handleBookAdding} >
-            <Form.Group>
-              <Form.Check 
-                label="Book read"
-              />
-            </Form.Group>
-            <Form.Group controlId="formGrade" >
-              <Form.Label>Grade</Form.Label>
-              <Form.Control 
-                as="select" 
-                value={rating}
-                onChange={e => setRating(e.target.value)} 
-              >
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-                <option value={5}>5</option>
-                <option value={6}>6</option>
-                <option value={7}>7</option>
-                <option value={8}>8</option>
-                <option value={9}>9</option>
-                <option value={10}>10</option>
-              </Form.Control>
-            </Form.Group>
+          <Form onSubmit={handleBookAdding} className="pt-4 text-left" >
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGrade" >
+                <Form.Label>Grade</Form.Label>
+                  <Form.Control 
+                    as="select" 
+                    value={rating}
+                    onChange={e => setRating(e.target.value)} 
+                  >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                    <option value={6}>6</option>
+                    <option value={7}>7</option>
+                    <option value={8}>8</option>
+                    <option value={9}>9</option>
+                    <option value={10}>10</option>
+                </Form.Control>
+              </Form.Group>
+            </Form.Row>
             <Form.Group controlId="formReview" >
               <Form.Label>Review</Form.Label>
               <Form.Control 
                 as="textarea"
+                rows={10}
                 value={review}
                 onChange={e => setReview(e.target.value)}
               />
             </Form.Group>
+            <Form.Group controlId="formBookRead">
+              <Form.Label>Book read</Form.Label>
+              <Form.Check onChange={() => setRead(!read)}/>
+            </Form.Group>
             <Button type="submit">Submit</Button>
           </Form>
-        </Collapse>  
-      </Row>
-
-      
+        </Collapse>      
     </Container>
     
   )
