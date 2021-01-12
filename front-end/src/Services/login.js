c7c8119cdcd8e14f5b0454c37ba4431f1357d4ad
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2021-01-12 16:36:43 +0200

    deleted frontend from repo

--- front-end/src/Services/login.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001'
-
-export const login = async (credentials) => {
-  const res = await axios.post(`${baseUrl}/api/login`, credentials)
-  return res.data
-}
-
