c4f764e67b0a1c4e6926fc005356b3f447dcb620
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2021-01-14 14:40:33 +0200

    editing book from backend

--- controllers/google_books.js
+++ controllers/google_books.js
@@ -49,4 +49,37 @@ googleBooksRouter.post('/', async (req, res) => {
   res.json(book.toJSON())
 })
 
+googleBooksRouter.put('/edit', async (req, res) => {
+  const body = req.body
+  const token = getToken(req)
+
+  const decodedToken = jwt.verify(token, config.SECRET)
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  const books = user.books
+  let bookIndex = -1
+  for (let i = 0; i < books.length; i ++) {
+    if (books[i].id === body.id) {
+      bookIndex = i
+      break
+    }
+  }
+  
+  if (bookIndex === -1) {
+    return res.status(404).json({ error: 'book not found' })
+  }
+
+  user.books[bookIndex].review = body.review
+  user.books[bookIndex].quotes = body.quotes
+  user.books[bookIndex].read = body.read
+  user.books[bookIndex].rating = body.rating
+
+  await user.save()
+
+  res.json(user.books[bookIndex].toJSON())
+})
+
 module.exports = googleBooksRouter
