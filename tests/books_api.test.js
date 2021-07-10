af9ffcbaff6b51bd06207e3ffc3d858e041ad846
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2021-07-10 12:51:43 +0300

    tests that require token are  working, new tests

--- tests/books_api.test.js
+++ tests/books_api.test.js
@@ -5,32 +5,114 @@ const api = supertest(app)
 const User = require('../models/user')
 const Book = require('../models/book')
 
+let token = null
+
+const initialBooks = [
+  {
+    title: 'Prince of Thorns',
+    author: 'Mark Lawrence',
+    grade: 4
+  },
+  {
+    title: 'Muumipeikko ja pyrstötähti',
+    author: 'Tove Jansson',
+    grade: 5
+  },
+  {
+    title: 'Sinuhe egyptiläinen',
+    author: 'Mika Waltari'
+  }
+]
 
 beforeAll(async () => {
   await User.deleteMany({})
-})
-
-test('creating a new user works', async() => {
   await api
     .post('/api/user')
-    .send({newUsername: 'Testaaja', newPassword: 'salasana'})
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('logging in works', async() => {
-  await api
+    .send({newUsername: 'stock_user', newPassword: 'password'})
+  await api  
     .post('/api/login')
-    .send({username: 'Testaaja', password: 'salasana'})
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    .send({username: 'stock_user', password: 'password'})
+    .then((res) => {
+      token = res.body.token
+    })
+  for (book in initialBooks) {
+    await api
+      .post('/api/googlebooks')
+      .send(book)
+  }
 })
 
-// test('adding new books works', async() => {
-//   await api
-//     .post('/api/googlebooks')
-//     .send({title: 'The Blade Itself' , author: 'Joe Abercrombie'})
-// })
+describe('login', () => {
+  test('login fails with wrong password', async() => {
+    await api
+    .post('/api/login')
+    .send({username: 'stock_user', password: 'wrong password'})
+    .expect(401)
+  })
+
+  test('login succeeds with correct credentials', async() => {
+    const resultUser = await api
+      .post('/api/login')
+      .send({username: 'stock_user', password: 'password'})
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    
+    expect(resultUser.body.username).toEqual('stock_user')
+  })  
+})
+
+describe('user creation', () => {
+  test('new user cannot be created if username already exists', async() => {
+    await api
+      .post('/api/user')
+      .send({newUsername: 'stock_user', newPassword: 'abcdefg'})
+      .expect(409)
+  })
+
+  test('username must be at least 5 characters long', async() => {
+    await api
+      .post('/api/user')
+      .send({newUsername: 'abcd', newPassword: 'abcdefg'})
+      .expect(500)
+
+    await api
+      .post('/api/user')
+      .send({newUsername: 'abcde', newPassword: 'abcdefg'})
+      .expect(200)
+  })
+
+  test('new user can be created', async() => {
+    await api
+      .post('/api/user')
+      .send({newUsername: 'Testaaja', newPassword: 'salasana'})
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+describe('user`s books', () => {
+  test('new book can be added', async() => {
+    await api
+      .post('/api/googlebooks')
+      .auth(token, { type: 'bearer' })
+      .send({title: 'The Blade Itself', author: 'Joe Abercrombie', rating: 4, rewiew: 'Great book!'})
+      .expect(200)
+  })
+
+  test('book cannot be added without title or author', async() => {
+    await api
+      .post('/api/googlebooks')
+      .auth(token, { type: 'bearer' })
+      .send({title: 'Muumilaakson marraskuu'})
+      .expect(500)
+
+    await api
+      .post('/api/googlebooks')
+      .auth(token, { type: 'bearer' })
+      .send({author: 'J.R.R Tolkien'})
+      .expect(500)
+  })
+})
 
 afterAll(() => {
   mongoose.connection.close()
