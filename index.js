2c49957f2d6cde36342ac9f12e3e36d64f596b0d
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2021-07-31 00:51:59 +0300

    deleted unnecessary code

--- index.js
+++ index.js
@@ -9,27 +9,3 @@ const PORT = config.PORT || 3001
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-// const app = express()
-// const cors = require('cors')
-
-// app.use(cors())
-// app.use(express.json())
-// const axios = require('axios')
-
-// const KEY = process.env.BOOKS_KEY
-// const PORT = process.env.PORT
-// const MONGODB_URI = process.env.MONGODB_URI
-
-// const baseUrl = `https://www.googleapis.com/books/v1/volumes?`
-
-// app.get('/:filter', async (request, response) => {
-//   const searchFilter = request.params.filter
-//   const url = `${baseUrl}q=${searchFilter}&maxResults=40&key=${KEY}`
-//   const books = await axios.get(url)
-//   response.send(books.data.items)
-// })
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
